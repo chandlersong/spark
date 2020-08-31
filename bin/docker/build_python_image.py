75ea83c5de704fa4b3b5f059b8cef1aa8a2b09ee
chandlersong <chandler605@gmail.com>
2020-08-31 21:03:20 +0800

    add script to build docker

---
+++ bin/docker/build_python_image.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+import subprocess
+
+
+
+def _find_workspace():
+    result = os.path.abspath(os.path.dirname(__file__))
+    while result != "/":
+        result = os.path.abspath(os.path.dirname(result))
+        if os.path.exists(os.path.join(result, "LICENSE")):
+            return result
+    return None
+
+
+class MyWorkSpace:
+
+    def __init__(self, custom_workspace=None):
+        if custom_workspace:
+            self.workspace = custom_workspace
+        else:
+            self.workspace = _find_workspace()
+
+
+def create_command_parser() -> argparse.Namespace:
+    result = argparse.ArgumentParser(description='manual to this script')
+    result.add_argument('--workspace', type=str, default=None)
+    result.add_argument('--app', type=str, default="simple_app")
+    return result.parse_args()
+
+if __name__ == '__main__':
+    args = create_command_parser()
+    workspace = MyWorkSpace(args.workspace)
+    print("workspace is {}".format(workspace.workspace))
+    commands = ["bin/docker-image-tool.sh",
+                 "-t",
+                 "v1",
+                 "-r",
+                 "jyt_spark",
+                 "-p",
+                 "resource-managers/kubernetes/docker/src/main/dockerfiles/spark/bindings/python/Dockerfile",
+                 "build"]
+    subprocess.run(commands, cwd=workspace.workspace)
+    commands = ["docker","tag","jyt_spark/spark-py:v1","localhost:8082/spark:v1"]
+    subprocess.run(commands, cwd=workspace.workspace)
+    ## need manully login before
+    commands = ["docker","push","localhost:8082/spark:v1"]
+    subprocess.run(commands, cwd=workspace.workspace)
+   
